4e9cf73b7147fe7ff4d15ac6ad65875619f7da36
pierrethierry <ngonoassogop@gmail.com>
2025-10-22 11:43:38 +0200

    test.2

--- audit_system.ps1
+++ audit_system.ps1
@@ -4,5 +4,5 @@ Utilisateur : ngono
 OS : Microsoft Windows 11 Famille
 Processeur : Intel(R) Celeron(R) J4105 CPU @ 1.50GHz
 RAM (Go) : 11.84
-Date : 22/10/2025 11:34
+Date : 22/10/2025 11:43
 ===========================
