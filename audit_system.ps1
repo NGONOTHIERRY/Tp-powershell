a8c339273ce1cbf8056ff1942a14421e1bd40d88
pierrethierry <ngonoassogop@gmail.com>
2025-10-22 11:39:09 +0200

    test

---
+++ audit_system.ps1
@@ -0,0 +1,8 @@
+﻿===== RAPPORT SYSTEME =====
+Machine : LESOLITERRIEN
+Utilisateur : ngono
+OS : Microsoft Windows 11 Famille
+Processeur : Intel(R) Celeron(R) J4105 CPU @ 1.50GHz
+RAM (Go) : 11.84
+Date : 22/10/2025 11:33
+===========================
