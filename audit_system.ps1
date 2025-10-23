a3e179cd808b3fe232938bf04b465eb425a49c08
NGONOTHIERRY <ngonoassogop@@example.com>
2025-10-23 10:01:40 +0200

    Dns-server

--- audit_system.ps1
+++ audit_system.ps1
@@ -1,8 +1,8 @@
 ﻿===== RAPPORT SYSTEME =====
-Machine : LESOLITERRIEN
-Utilisateur : ngono
-OS : Microsoft Windows 11 Famille
-Processeur : Intel(R) Celeron(R) J4105 CPU @ 1.50GHz
-RAM (Go) : 11.84
-Date : 22/10/2025 11:43
+Machine : SCDN2
+Utilisateur : Administrateur
+OS : Microsoft Windows Server 2022 Standard
+Processeur : 12th Gen Intel(R) Core(TM) i7-12700 12th Gen Intel(R) Core(TM) i7-12700
+RAM (Go) : 4
+Date : 23/10/2025 09:46
 ===========================
--- test.ps1
+++ test.ps1
@@ -1,19 +1,24 @@
-# audit_system.ps1
-$Date = Get-Date -Format "dd/MM/yyyy HH:mm"
-$ComputerName = $env:COMPUTERNAME
-$User = $env:USERNAME
-$OS = (Get-ComputerInfo).OsName
-$CPU = (Get-WmiObject Win32_Processor).Name
-$RAM = (Get-WmiObject Win32_ComputerSystem).TotalPhysicalMemory / 1GB
+$DnsServerName = "srvc.dc1"
+$IpAddress = "192.168.147.10"
+$Forwarders = @("8.8.8.8", "8.8.4.4")
+
+    Install-WindowsFeature DNS -IncludeManagementTools
+
+New-NetIPAddress -InterfaceAlias $adapter.Name -IPAddress $IpAddress -PrefixLength 24 -DefaultGateway "192.168.147.1"
+
+Rename-Computer -NewName $DnsServerName 
+Restart-Computer 
+
+Import-Module DNSServer
+
 $Rapport = @"
 ===== RAPPORT SYSTEME =====
-Machine : $ComputerName
-Utilisateur : $User
-OS : $OS
-Processeur : $CPU
-RAM (Go) : $([math]::Round($RAM,2))
-Date : $Date
+ srv1.dc1 : $DnsServerName
+ Adresse IP : $IpAddress
+Ipadress : $IpAddress
+Forwarders : $( $Forwarders -join ", " )
+DefaulGeteway : $defaultGateway
 ===========================
 "@
-$Rapport | Out-File "./audit_system.ps1" -Encoding UTF8
-Write-Host "Rapport généré dans /audit_system.ps1" -ForegroundColor Green
+$Rapport | Out-File "./audit_system.csv"Encoding UTF8
+Write-Host "Rapport généré dans /audit_system.cs1"ForegroundColor Green
